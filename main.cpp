09d4b6755b596cb1c4308435663efa3770b08327
Bibek Bhattarai <bibekjodd@outlook.com>
2022-11-15 14:33:57 +0545

    tree

--- main.cpp
+++ main.cpp
@@ -1,114 +1,172 @@
 #include <iostream>
 #include <vector>
 #include <stack>
-#include <algorithm>
+#include <queue>
 using namespace std;
 
-int precedence(char ch)
+class Node
 {
-    if (ch == '^')
-        return 4;
+public:
+    int val;
+    Node *left;
+    Node *right;
 
-    else if (ch == '/' || ch == '*')
-        return 3;
+    Node()
+    {
+        val = 0;
+        left = NULL;
+        right = NULL;
+    }
 
-    else if (ch == '+' || ch == '-')
-        return 2;
+    Node(int val)
+    {
+        this->val = val;
+        left = NULL;
+        right = NULL;
+    }
+};
 
+void preorder(Node *root)
+{
+    if (!root)
+        return;
+    cout << root->val << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(Node *root)
+{
+    if (!root)
+        return;
+    inorder(root->left);
+    cout << root->val << " ";
+    inorder(root->right);
+}
+
+void postorder(Node *root)
+{
+    if (!root)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->val << " ";
+}
+
+void levelorder(Node *root)
+{
+    if (!root)
+        return;
+
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        Node *front = q.front();
+        q.pop();
+        if (front)
+        {
+            cout << front->val << " ";
+
+            if (front->left)
+                q.push(front->left);
+
+            if (front->right)
+                q.push(front->right);
+        }
+        else if (!q.empty())
+        {
+            q.push(NULL);
+        }
+    }
+    cout << "\n";
+}
+
+// total nodes in a binary tree
+int countNodes(Node *root)
+{
+    if (!root)
+        return 0;
+    return countNodes(root->left) + countNodes(root->right) + 1;
+}
+
+// sum of all nodes in a binary tree
+int sumOfAllNodes(Node *root)
+{
+    if (!root)
+        return 0;
+    return sumOfAllNodes(root->left) + sumOfAllNodes(root->right) + root->val;
+}
+
+int height(Node *root)
+{
+    if (!root)
+        return 0;
+
+    return max(height(root->left), height(root->right)) + 1;
+}
+
+void sumReplacement(Node *&root)
+{
+    if (!root)
+        return;
+
+    sumReplacement(root->left);
+    sumReplacement(root->right);
+
+    if (root->left)
+        root->val += root->left->val;
+
+    if (root->right)
+        root->val += root->right->val;
+}
+
+// balanced height in a binary tree
+bool balancedHeight(Node *root)
+{
+    if (!root)
+        return 1;
+    if (!balancedHeight(root->left))
+        return 0;
+
+    if (!balancedHeight(root->right))
+        return 0;
+
+    int lHeight = height(root->left);
+    int rHeight = height(root->right);
+    if (abs(lHeight - rHeight) > 1)
+        return 0;
     return 1;
-}
-
-string infixToPostfix(string s)
-{
-    stack<char> st;
-    string ans = "";
-    int n = s.length();
-    for (int i = 0; i < n; i++)
-    {
-        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '0' && s[i] <= '9'))
-            ans.push_back(s[i]);
-        else if (s[i] == '(')
-            st.push('(');
-        else if (s[i] == ')')
-        {
-            while (!st.empty() && st.top() != '(')
-            {
-                ans.push_back(st.top());
-                st.pop();
-            }
-            if (!st.empty())
-                st.pop();
-        }
-        else
-        {
-            while (!st.empty() && precedence(st.top()) > precedence(s[i]))
-            {
-                ans.push_back(st.top());
-                st.pop();
-            }
-            st.push(s[i]);
-        }
-    }
-    while (!st.empty())
-    {
-        char ch = st.top();
-        if (
-            (ch >= 'a' && ch <= 'z') ||
-            (ch >= 'A' && ch <= 'Z') ||
-            (ch >= '0' && ch <= '9') ||
-            (ch == '^' || ch == '*' || ch == '/' || ch == '+' || ch == '-'))
-            ans.push_back(st.top());
-        st.pop();
-    }
-    return ans;
-}
-
-string infixToPrefix(string s)
-{
-    reverse(s.begin(), s.end());
-    int n = s.length();
-    for (int i = 0; i < n; i++)
-        if (s[i] == '(')
-            s[i] = ')';
-        else if (s[i] == ')')
-            s[i] = '(';
-    string ans = infixToPostfix(s);
-    reverse(ans.begin(), ans.end());
-    return ans;
-}
-
-bool balancedParenthesis(string s)
-{
-    int n = s.length();
-    stack<char> st;
-    for (int i = 0; i < n; i++)
-    {
-        if (s[i] == '(' || s[i] == '{' || s[i] == '[')
-            st.push(s[i]);
-        else if (s[i] == ')' || s[i] == '}' || s[i] == ']')
-        {
-            if (st.empty())
-                return 0;
-            if (s[i] == ')' && st.top() != '(')
-                return 0;
-            if (s[i] == '}' && st.top() != '{')
-                return 0;
-            if (s[i] == ']' && st.top() != '[')
-                return 0;
-            st.pop();
-        }
-    }
-    return st.empty();
 }
 
 int main()
 {
-    // string s = "(a-b/c)*(a/k-l)";
-    string s;
-    cin >> s;
-    // cout << "Prefix => " << infixToPrefix(s) << "\n\n";
-    // cout << "Postfix => " << infixToPostfix(s) << "\n";
-    cout << balancedParenthesis(s);
+    Node *root = NULL;
+    root = new Node(0);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    // preorder(root);
+    // cout << "\n";
+    // inorder(root);
+    // cout << "\n";
+    // postorder(root);
+    // cout << "\n";
+
+    levelorder(root);
+    // sumReplacement(root);
+    // cout << countNodes(root) << "\n";
+    // cout << sumOfAllNodes(root) << "\n";
+
+    // cout << height(root);
+
+    cout << balancedHeight(root) << "\n";
 
     return 0;
 }
