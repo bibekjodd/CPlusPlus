8e3b893ab47be4c9634264258e269e4cc633ac66
Bibek Bhattarai <bibekjodd@outlook.com>
2022-11-16 10:19:59 +0545

    bst

--- main.cpp
+++ main.cpp
@@ -210,18 +210,104 @@ bool find(Node *root, int key)
     return 0;
 }
 
+Node *LCA(Node *root, int n1, int n2)
+{
+    if (!root)
+        return NULL;
+
+    if (root->val == n1 || root->val == n2)
+        return root;
+
+    Node *leftLCA = LCA(root->left, n1, n2);
+    Node *rightLCA = LCA(root->right, n1, n2);
+
+    if (leftLCA && rightLCA)
+        return root;
+
+    if (leftLCA)
+        return leftLCA;
+
+    if (rightLCA)
+        return rightLCA;
+
+    return NULL;
+}
+
+// Binary Search Tree
+Node *insertBST(Node *root, int val)
+{
+    if (!root)
+        return new Node(val);
+
+    if (val < root->val)
+        root->left = insertBST(root->left, val);
+    else
+        root->right = insertBST(root->right, val);
+
+    return root;
+}
+
+bool searchBST(Node *root, int key)
+{
+    if (!root)
+        return 0;
+
+    if (root->val == key)
+        return 1;
+
+    if (key < root->val)
+        return searchBST(root->left, key);
+    else
+        return searchBST(root->right, key);
+}
+
+// delete in binary search tree
+
+Node *inorderSucc(Node *root)
+{
+    while (root && root->left)
+        root = root->left;
+    return root;
+}
+
+Node *deleteBST(Node *root, int key)
+{
+    if (!root)
+        return NULL;
+
+    if (key < root->val)
+        root->left = deleteBST(root->left, key);
+
+    else if (key > root->val)
+        root->right = deleteBST(root->right, key);
+
+    else
+    {
+        if (!root->left)
+            return root->right;
+
+        else if (!root->right)
+            return root->left;
+
+        Node *temp = inorderSucc(root->right);
+        root->val = temp->val;
+        root->right = deleteBST(root->right, temp->val);
+    }
+    return root;
+}
+
 int main()
 {
     Node *root = NULL;
-    root = new Node(0);
-    root->left = new Node(2);
-    root->right = new Node(3);
-    root->left->left = new Node(4);
-    root->left->left->right = new Node(8);
-    root->left->right = new Node(5);
-    root->right->left = new Node(6);
-    root->right->left->left = new Node(9);
-    root->right->right = new Node(7);
+    // root = new Node(0);
+    // root->left = new Node(2);
+    // root->right = new Node(3);
+    // root->left->left = new Node(4);
+    // root->left->left->right = new Node(8);
+    // root->left->right = new Node(5);
+    // root->right->left = new Node(6);
+    // root->right->left->left = new Node(9);
+    // root->right->right = new Node(7);
 
     // preorder(root);
     // cout << "\n";
@@ -230,7 +316,7 @@ int main()
     // postorder(root);
     // cout << "\n";
 
-    levelorder(root);
+    // levelorder(root);
     // sumReplacement(root);
     // cout << countNodes(root) << "\n";
     // cout << sumOfAllNodes(root) << "\n";
@@ -240,7 +326,34 @@ int main()
     // cout << balancedHeight(root) << "\n";
     // leftView(root);
     // rightView(root);
-    cout << find(root, 7) << "\n";
+    // cout << find(root, 7) << "\n";
+    // Node *lca = LCA(root, 6, 7);
+    // if (lca)
+    //     cout << lca->val << "\n";
+    // else
+    //     cout << "NO LCA exists for the node\n";
+
+    // binary search tree
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        root = insertBST(root, a[i]);
+    }
+
+    inorder(root);
+    cout << "\n";
+
+    // cout << searchBST(root, 9) << "\n";
+    deleteBST(root, 1);
+    deleteBST(root, 2);
+    deleteBST(root, 9);
+    deleteBST(root, 8);
+    deleteBST(root, 6);
+    inorder(root);
+    cout << "\n";
 
     return 0;
 }
